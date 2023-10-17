9239642032de309d04ef5c3d2115ff9d55ec6a0d
Shayan165 <shayanbhatkal786@gmail.com>
2023-10-17 21:16:42 +0530

    disabled button

--- scr.js
+++ scr.js
@@ -30,11 +30,10 @@ const popoverList = [...popoverTriggerList].map(
 
 // form validation
 
-document.getElementById("submit-btn").addEventListener('click',function(){
+document.getElementById("submit-btn").addEventListener("click", function () {
   let FullName = document.getElementById("inputName").value;
   let Email = document.getElementById("inputEmail").value;
   let Message = document.getElementById("inputMessage").value;
-
   const NameRGEX = /^[A-Za-z ]{2,30}$/;
   const EmailRGEX = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+\.[a-zA-z]{2,3}$/;
   const MessageRGEX = /.*[A-Za-z]+.*/;
@@ -43,36 +42,51 @@ document.getElementById("submit-btn").addEventListener('click',function(){
   let EmailResult = EmailRGEX.test(Email);
   let MessageResult = MessageRGEX.test(Message);
 
+  const validationMessages = [];
+  const successMessage =
+    "Your message has been received. I appreciate your interest and potential opportunities.\n As a professional in the industry,\n I value your outreach. :)";
 
-const validationMessages = [];
-const successMessage = "Your message has been received. I appreciate your interest and potential opportunities.\n As a professional in the industry,\n I value your outreach. :)"
-
-if (!NameResult) {
-    validationMessages.push('Please enter a valid Name.');
+  if (!NameResult) {
+    validationMessages.push("Please enter a valid Name.");
   }
 
   if (!EmailResult) {
-    validationMessages.push('Please enter a valid Email Address.');
+    validationMessages.push("Please enter a valid Email Address.");
   }
 
-
   if (!MessageResult) {
-    validationMessages.push('Message cannot be empty.');
+    validationMessages.push("Message cannot be empty.");
   }
 
   if (validationMessages.length > 0) {
-    const validationMessage = validationMessages.join('\n');
-    document.getElementById('validationMessage').innerText
-     = validationMessage;
-    $('#validationModal').modal('show');
+    const validationMessage = validationMessages.join("\n");
+    document.getElementById("validationMessage").innerText = validationMessage;
+    $("#validationModal").modal("show");
   }
 
-
-  if(NameResult && EmailResult && MessageResult){
-    document.getElementById('exampleModalLabel').innerText = "Thank you for getting in touch!"
-    document.getElementById('validationMessage').innerText
-    = successMessage;
-    $('#validationModal').modal('show');
+  if (NameResult && EmailResult && MessageResult) {
+    document.getElementById("exampleModalLabel").innerText =
+      "Thank you for getting in touch!";
+    document.getElementById("validationMessage").innerText = successMessage;
+    $("#validationModal").modal("show");
   }
+});
 
-})
+// disable form submit button until required fields are entered
+
+function enableSubmit() {
+  let inputs = document.getElementsByClassName("required");
+
+  let btn = document.querySelector('button[type="submit"]');
+
+  let isValid = true;
+  for (let i = 0; i < inputs.length; i++) {
+    let changedInput = inputs[i];
+
+    if (changedInput.value.trim() === "" || changedInput.value === null) {
+      isValid = false;
+      break;
+    }
+  }
+  btn.disabled = !isValid;
+}
